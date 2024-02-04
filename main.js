4f126db41d662309ca7408f5f943d464ce0a22c0
Ksushabay <Ksushabay74@gmail.com>
2024-02-04 14:03:46 +0600

    new board two

---
+++ main.js
@@ -0,0 +1,46 @@
+const board=document.querySelector("#board");
+const numbers=500;
+const colors =[
+  'red',
+    'orange',
+    'yellow',
+    'green',
+    'blue',
+    'indigo',
+    'violet',
+    'pink',
+    'white',
+    'black'
+];
+for (let i=0;i<numbers;i++) {
+  const block = document.createElement('div');
+  const open =document.querySelector('.open');
+  const close =document.querySelector('.close');
+
+  open.addEventListener('click',()=> setColors(block));
+  close.addEventListener('click',()=> removeColors(block));
+
+  block.classList.add('block')
+
+  block.addEventListener('mouseover',()=>{
+    setColors(block)
+  });
+  block.addEventListener('mouseleave',()=>{
+    removeColors(block)
+  });
+
+  board.append(block)
+}
+function getColorsRandom (){
+  const index =Math.round(Math.random()*colors.length)
+  return colors[index]
+}
+function setColors (block){
+  const colors = getColorsRandom();
+  block.style.background = colors;
+  block.style.boxShadow = `0 0 10px ${colors},0 010px ${colors}`
+}
+function removeColors(block){
+  block.style.background = "#1d1d1d";
+  block.style.boxShadow = '0 0 2px #000'
+}
